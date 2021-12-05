ebf646d4d3e2798364495b64795737ed75ff8f81
Frédéric Montet <frederic.montet@vast.ch>
2021-12-05 14:55:40 +0100

    remove KeeWeb

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,22 +13,35 @@ alias cls='clear' # Good 'ol Clear Screen command
 
 alias lg='lazygit'
 
+alias jl='jupyter lab'
+alias jn='jupyter notebook'
+
+alias tb.ligntning=tensorboard --logdir ./lightning_logs
+alias tb.darts=tensorboard --logdir ./.darts/runs
+
 # Project aliases
 #################
 
 # Vast Outdoor GmbH
-alias vast.v1='cd ~/src/org/vast-outdoor/vast-ch'
-alias vast.v2='cd ~/src/org/vast-outdoor/vast-ch-v2'
+alias vast="cd /Users/frederic.montet/'frederic.montet@vast.ch - Google Drive'/'My Drive'/org/vast-software"
+alias vast.src='cd ~/src/org/vast-software'
+alias vast.ch='cd ~/src/org/vast-software/vast-ch'
 
 # Thesis
-alias thesis.report='cd ~/src/org/heia-fr/thesis-report && code .'
+alias thesis='cd ~/src/org/heia-fr/thesis-report && code .'
 alias thesis.experiments='cd ~/src/org/heia-fr/thesis-experiments && pipenv shell'
 alias thesis.publications='cd ~/src/org/heia-fr/thesis-publications && code .'
 
 # HEIA-FR
 alias heia.f4='cd ~/src/org/heia-fr/facility-4.0 && pipenv shell'
+alias heia.f4.app='cd ~/src/org/heia-fr/facility-4.0_app && pipenv shell'
 alias heia.a4='cd ~/src/org/heia-fr/assainissement-4.0 && pipenv shell'
+alias heia.a4.app='cd ~/src/org/heia-fr/assainissement-4.0_app && pipenv shell'
 alias heia.ta='cd ~/src/org/heia-fr/timeatlas && pipenv shell'
+
+# BBData
+alias bbdata.ssh='ssh bbdata-admin@bbdata.smartlivinglab.ch -p 2201'
+alias bbdata.sshuttle='sshuttle --dns -r bbdata-admin@bbdata.smartlivinglab.ch:2201 10.10.0.0/24'
 
 # Personal
 alias project.tars='cd ~/src/project/tars && pipenv shell'
