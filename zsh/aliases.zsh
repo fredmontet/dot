cc9748f08d352af01c011d2ae03a02a17de1ca87
Frédéric Montet <fredmontet@gmail.com>
2022-11-04 10:59:48 +0100

    Just an update with many stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,6 @@ alias reload='. ~/.zshrc'
 
 alias ..='cd ..'
 alias ...='cd ../..'
-
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias src='cd ~/src'
@@ -13,11 +12,16 @@ alias src='cd ~/src'
 # Software aliases
 ##################
 
-alias lg='lazygit'
-
+# Editors
 alias jl='jupyter lab'
 alias jn='jupyter notebook'
+alias ide='idea'
+alias ds='dataspell'
+alias e='code'
 
+# Dev tools
+alias k='kubectl'
+alias lg='lazygit'
 alias tb.ligntning=tensorboard --logdir ./lightning_logs
 alias tb.darts=tensorboard --logdir ./.darts/runs
 
@@ -25,29 +29,31 @@ alias tb.darts=tensorboard --logdir ./.darts/runs
 #################
 
 # Vast Outdoor GmbH
-alias vast="cd /Users/frederic.montet/'frederic.montet@vast.ch - Google Drive'/'My Drive'/org/vast-software"
-alias vast.src='cd ~/src/org/vast-software'
-alias vast.ch='cd ~/src/org/vast-software/vast-ch'
+alias vast.ch='cd ~/src/vast-ch'
 
 # Thesis
-alias thesis='cd ~/src/org/heia-fr/thesis-report && code .'
-alias thesis.experiments='cd ~/src/org/heia-fr/thesis-experiments && pipenv shell'
-alias thesis.publications='cd ~/src/org/heia-fr/thesis-publications && code .'
+alias thesis.phd='cd ~/src/thesis-phd && code .'
+alias thesis.master='cd ~/src/thesis-master && code .'
+alias thesis.experiments='cd ~/src/thesis-experiments && pipenv shell'
+alias thesis.publications='cd ~/src/thesis-publications && code .'
 
 # HEIA-FR
-alias heia.f4='cd ~/src/org/heia-fr/facility-4.0 && pipenv shell'
-alias heia.f4.app='cd ~/src/org/heia-fr/facility-4.0_app && pipenv shell'
-alias heia.a4='cd ~/src/org/heia-fr/assainissement-4.0 && pipenv shell'
-alias heia.a4.app='cd ~/src/org/heia-fr/assainissement-4.0_app && pipenv shell'
-alias heia.ta='cd ~/src/org/heia-fr/timeatlas && pipenv shell'
+alias heia.f4='cd ~/src/facility-4.0 && pipenv shell'
+alias heia.f4.app='cd ~/src/facility-4.0_app && pipenv shell'
+alias heia.a4='cd ~/src/assainissement-4.0 && pipenv shell'
+alias heia.a4.app='cd ~/src/assainissement-4.0_app && pipenv shell'
+alias heia.ta='cd ~/src/timeatlas && pipenv shell'
 
 # BBData
 alias bbdata.ssh='ssh bbdata-admin@bbdata.smartlivinglab.ch -p 2201'
 alias bbdata.sshuttle='sshuttle --dns -r bbdata-admin@bbdata.smartlivinglab.ch:2201 10.10.0.0/24'
 
 # Personal
-alias project.tars='cd ~/src/project/tars && pipenv shell'
-alias project.fredmontet='cd ~/src/project/fredmontet.com && code .'
+alias p.jam='cd ~/src/jam && poetry shell'
+alias p.tars='cd ~/src/tars && pipenv shell'
+alias p.blog='cd ~/src/blog'
+alias p.experiments='cd ~/src/experiments && make activate'
+alias p.note='cd ~/src/note && pipenv shell'
 
 # Project templates
-alias new.ds='cookiecutter https://github.com/drivendata/cookiecutter-data-science'
+alias new.ds='cookiecutter https://github.com/fredmontet/template-data-science'
